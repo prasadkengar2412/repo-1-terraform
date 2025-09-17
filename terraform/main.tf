aa1a23fda782c17608ea421b0673ce9378d86ae8
prasadkengar2412 <109298995+prasadkengar2412@users.noreply.github.com>
2025-09-17 20:30:55 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -69,7 +69,7 @@ resource "aws_cognito_user_pool_client" "apps" {
   callback_urls = lookup(each.value, "redirect_urls", [])
   logout_urls   = lookup(each.value, "logout_urls", [])
 
-  access_token_validity  = lookup(each.value, "access_token_validity", null) != null ? lookup(each.value.access_token_validity, "value", 5) : 5
+  access_token_validity  = lookup(each.value, "access_token_validity", null) != null ? lookup(each.value.access_token_validity, "value", 1) : 1
   id_token_validity      = lookup(each.value, "id_token_validity", null) != null ? lookup(each.value.id_token_validity, "value", 1) : 1
   refresh_token_validity = lookup(each.value, "refresh_token_validity", null) != null ? lookup(each.value.refresh_token_validity, "value", 30) : 30
 
